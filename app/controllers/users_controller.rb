0039fe317a66e9e307c5d9790898e4caa4d35dad
J Seymour <JustJakeSeymour@gmail.com>
2023-02-14 16:07:19 -0700

    part one

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,45 @@
 class UsersController <ApplicationController 
-    def new 
-        @user = User.new()
+  def new 
+    @user = User.new()
+  end 
+
+  def show 
+    @user = User.find(params[:id])
+  end 
+
+  def create 
+    user = User.create(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else  
+      flash[:error] = user.errors.full_messages.to_sentence
+      redirect_to register_path
     end 
-
-    def show 
-        @user = User.find(params[:id])
+  end 
+      
+  def login_form
+    
+  end 
+  
+  def login_user
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to "/users/#{user.id}"
+    else 
+      flash[:error] = "Bad Credentials, try again."
+      redirect_to "/login" 
     end 
+  end 
 
-    def create 
-        user = User.create(user_params)
-        if user.save
-            redirect_to user_path(user)
-        else  
-            flash[:error] = user.errors.full_messages.to_sentence
-            redirect_to register_path
-        end 
-    end 
+  def logout_user
+    session[:user_id] = nil
+    redirect_to "/"
+  end
+private 
 
-    def login_form
-
-    end 
-
-    def login_user
-        user = User.find_by(email: params[:email])
-        if user && user.authenticate(params[:password])
-            redirect_to "/users/#{user.id}"
-        else 
-            flash[:error] = "Bad Credentials, try again."
-            redirect_to "/login" 
-        end 
-    end 
-
-    private 
-
-    def user_params 
-        params.require(:user).permit(:name, :email, :password)
-    end 
+  def user_params 
+    params.require(:user).permit(:name, :email, :password)
+  end 
 end 
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -5,31 +5,62 @@ RSpec.describe "User Log In" do
     user = User.create(name: 'User One', email: 'email@example.com', password: 'password123')
 
     visit "/"
-
     click_link "Log In"
 
     fill_in :email, with:'email@example.com'
     fill_in :password, with: 'password123'
     click_button 'Log In'
-
+    
     expect(current_path).to eq("/users/#{user.id}")
     expect(page).to have_content("User One's Dashboard")
   end 
-
+  
   it 'does not log in user with incorrect credentials' do 
     user = User.create(name: 'User One', email: 'email@example.com', password: 'password123')
-
+    
     visit "/"
-
+    
     click_link "Log In"
-
+    
     fill_in :email, with:'email@example.com'
     fill_in :password, with: 'nottherightpassword'
     click_button 'Log In'
-
-
+    
+    
     expect(current_path).to eq("/login")
     expect(page).to have_content("Bad Credentials, try again.")
     
+  end
+  
+  describe "User Log Out" do
+    before :each do
+      @user = User.create(name: 'User One', email: 'email@example.com', password: 'password123')
+    
+      visit "/"
+      click_link "Log In"
+    
+      fill_in :email, with:'email@example.com'
+      fill_in :password, with: 'password123'
+      click_button 'Log In'
+    end
+
+    it 'does not display create user or log in link if user is logged in' do
+      visit "/"
+      
+      expect(page).to_not have_link("Log In")
+      expect(page).to_not have_button("Create New User")
+      expect(page).to have_link("Log Out")
+    end
+    
+    it 'can log a user out when link is clicked' do
+      visit "/"
+      
+      click_link "Log Out"
+      
+      expect(current_path).to eq "/"
+
+      expect(page).to have_link("Log In")
+      expect(page).to have_button("Create New User")
+    end
   end
 end
